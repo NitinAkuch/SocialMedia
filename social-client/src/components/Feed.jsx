03a45314ebf0613ca4b3af27b2e2f2577ec7d156
NitinAkuch <110688610+NitinAkuch@users.noreply.github.com>
2023-10-05 00:06:41 +0530

    Post Component added

--- social-client/src/components/Feed.jsx
+++ social-client/src/components/Feed.jsx
@@ -7,11 +7,25 @@ import { Posts } from "../dummyData";
 const Container = styled.div`
   flex: 5.5;
 `;
+// const PostContainer = styled.div`
+//   overflow-y: scroll;
+//   /* position: sticky; */
+//   &::-webkit-scrollbar {
+//     width: 5px;
+//   }
+//   &::-webkit-scrollbar-track {
+//     background-color: #f9f6f6;
+//   }
+//   &::-webkit-scrollbar-thumb {
+//     background-color: rgb(233, 230, 230);
+//   }
+// `;
 const Feed = () => {
   return (
     <>
       <Container>
         <Share />
+
         {Posts.map((p) => (
           <Post key={p.id} post={p} />
         ))}
--- social-client/src/components/Post.jsx
+++ social-client/src/components/Post.jsx
@@ -1,41 +1,118 @@
-import React from "react";
+import { MoreVert, ThumbDownOffAlt, ThumbUp } from "@mui/icons-material";
+import React, { useState } from "react";
 import styled from "styled-components";
+import { Users } from "../dummyData";
 
+const Container = styled.div`
+  width: 100%;
+  border-radius: 10px;
+  margin: 30px 0px;
+  -webkit-box-shadow: 0px 0px 16px -8px rgba(0, 0, 0, 0.68);
+  box-shadow: 0px 0px 16px -8px rgba(0, 0, 0, 0.68);
+`;
+const Wrapper = styled.div`
+  padding: 10px;
+`;
+const Top = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+const TopLeft = styled.div`
+  display: flex;
+  align-items: center;
+`;
+const ProfileImage = styled.img`
+  width: 32px;
+  height: 32px;
+  border-radius: 50%;
+  object-fit: cover;
+`;
+const Username = styled.span`
+  font-size: 15px;
+  font-weight: 500;
+  margin: 0 10px;
+`;
+const PostDate = styled.span`
+  font-size: 12px;
+`;
+const TopRight = styled.div``;
+const Center = styled.div`
+  margin: 20px 0px;
+`;
+const PostImage = styled.img`
+  margin-top: 20px;
+  width: 100%;
+  max-height: 500px;
+  object-fit: contain;
+`;
+const Bottom = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+const Reactions = styled.span`
+  display: flex;
+  align-items: center;
+`;
+
+const Likes = styled.span`
+  font-size: 15px;
+  margin-left: 10px;
+`;
+const BottomComment = styled.span`
+  cursor: pointer;
+  border-bottom: 1px dashed gray;
+  font-size: 15px;
+`;
 const Post = ({ post }) => {
-  const Container = styled.div``;
-  const Wrapper = styled.div``;
-  const Top = styled.div``;
-  const TopLeft = styled.div``;
-  const TopRight = styled.div``;
-  const Center = styled.div``;
-  const ProfileImage = styled.img``;
-  const Username = styled.span``;
-  const PostDate = styled.span``;
-  const PostImage = styled.img``;
-  const Bottom = styled.div``;
-  const Reactions = styled.span``;
-  const BottomIcon = styled.span``;
-  const BottomComment = styled.span``;
+  const [like, setLike] = useState(post.like);
+  const [isLiked, setIsLiked] = useState(false);
+
+  const likeHandler = () => {
+    setLike(isLiked ? like - 1 : like + 1);
+    setIsLiked(!isLiked);
+  };
   return (
     <>
       <Container>
         <Wrapper>
           <Top>
             <TopLeft>
-              <ProfileImage />
-              <Username></Username>
-              <PostDate></PostDate>
+              <ProfileImage
+                src={
+                  Users.filter((u) => u.id === post?.userId)[0].profilePicture
+                }
+                alt=""
+              />
+              <Username>
+                {Users.filter((u) => u.id === post?.userId)[0].username}
+              </Username>
+              <PostDate>{post.date}</PostDate>
             </TopLeft>
-            <TopRight></TopRight>
+            <TopRight>
+              <MoreVert />
+            </TopRight>
           </Top>
           <Center>
-            <PostImage />
+            <PostImage src={post.photo} alt="" />
           </Center>
           <Bottom>
             <Reactions>
-              <BottomIcon></BottomIcon>
+              <ThumbUp
+                htmlColor="blue"
+                sx={{
+                  width: "24px",
+                  height: "24px",
+                  marginRight: "5px",
+                  cursor: "pointer",
+                }}
+                onClick={likeHandler}
+              />
+              <ThumbDownOffAlt htmlColor="blue" onClick={likeHandler} />
+              <Likes>{like} Peoples have liked it!</Likes>
             </Reactions>
-            <BottomComment></BottomComment>
+            <BottomComment>{post.comment}comments</BottomComment>
           </Bottom>
         </Wrapper>
       </Container>
